b81b525eab91d9e6026e839aba2aa10688d901e9
Ehsanul Hoque <ehsanul@ehsanul.com>
2024-12-08 17:37:09 -0800

    record sessions at 0.5 fps

--- stream.py
+++ stream.py
@@ -1,24 +1,58 @@
 import os
 import cv2
+import queue
+import threading
+import time
 
 # read username and password from dotenv file
-from dotenv import load_dotenv # type: ignore
+from dotenv import load_dotenv
+from ksuid import Ksuid # type: ignore
 load_dotenv()
 
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
+camera_ip = os.getenv("CAMERA_IP")
 
-cap = cv2.VideoCapture(f'rtsp://{username}:{password}@10.0.0.176:554/Preview_01_main')
+# bufferless VideoCapture
+class VideoCapture:
+    def __init__(self, rtsp_url):
+        self.cap = cv2.VideoCapture(rtsp_url)
+        self.q = queue.Queue()
+        t = threading.Thread(target=self._reader)
+        t.daemon = True
+        t.start()
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    def _reader(self):
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            if not self.q.empty():
+                try:
+                    self.q.get_nowait()   # discard previous (unprocessed) frame
+                except queue.Empty:
+                    pass
+            self.q.put(frame)
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    def read(self):
+        return self.q.get()
+
+# Create VideoCapture instance with RTSP URL
+rtsp_url = f'rtsp://{username}:{password}@{camera_ip}:554/Preview_01_main'
+cap = VideoCapture(rtsp_url)
+
+session_id = Ksuid()
+session_dir = f'captures/{session_id}'
+os.makedirs(session_dir, exist_ok=True)
+
+num = 0
+while True:
+    num += 1
+    time.sleep(2)
+    frame = cap.read()
+    cv2.imshow("frame", frame)
+    cv2.imwrite(f'{session_dir}/frame{num:03d}.jpg', frame)
+    if chr(cv2.waitKey(1)&255) == 'q':
         break
 
-# When everything done, release the capture
-cap.release()
 cv2.destroyAllWindows()
